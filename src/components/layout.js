58fe5801c5368e0f2326b9318cf3be5df5b246fb
Yusinto Ngadiman <yusinto@gmail.com>
2018-08-08 16:12:00 +1000

    Added styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
-
+import {StaticQuery, graphql} from 'gatsby'
+import styled, {injectGlobal} from 'styled-components'
 import Header from './header'
-import './layout.css'
 
-const Layout = ({ children, data }) => (
+injectGlobal`
+  html {
+    font-family: sans-serif;
+  }
+  
+  body {
+    margin: 0;
+  }
+`
+
+const RootDiv = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`
+
+const Layout = ({children, data}) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -19,26 +34,19 @@ const Layout = ({ children, data }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          {name: 'description', content: 'Sample'},
+          {name: 'keywords', content: 'sample, something'},
+        ]}
+      >
+        <html lang="en"/>
+      </Helmet>
+      <Header siteTitle={data.site.siteMetadata.title}/>
+      <RootDiv>
+        {children}
+      </RootDiv>
       </>
     )}
   />
