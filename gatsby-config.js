aa15324f509b0c61fc2e2bd6510a9173691b7d3a
Yusinto Ngadiman <yusinto@gmail.com>
2018-08-23 19:47:28 +1000

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,5 +66,19 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-75763786-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: false,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
