b9d90cf7dfadfd600d1b6ba121a3c81f91dbeea3
Yusinto Ngadiman <yusinto@gmail.com>
2018-08-25 10:02:19 +1000

    Prettified tag list

--- src/components/tagList.js
+++ src/components/tagList.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const List = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  letter-spacing: 0;
+  font-size: 15px;
+  font-weight: 400;
+  font-style: normal;
+`
+const ListItem = styled.li`
+    display: inline-block;
+    border: none;
+    color: rgba(0,0,0,.68);
+    background: rgba(0,0,0,.05);
+    border-radius: 3px;
+    margin: 8px;
+    padding: 0px 10px;
+`
 
 export default ({tags}) => (
-  <ul>
+  <List>
     {
       tags.map(t => {
-        return <li key={t}>{t}</li>
+        return <ListItem key={t}>{t}</ListItem>
       })
     }
-  </ul>
+  </List>
 )
