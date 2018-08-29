76d128a85c44b022db4d427c70242b0226190499
Yus <yusinto@gmail.com>
2018-08-29 14:05:37 +1000

    a

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon-512x512.png', // This path is relative to the root of the site.
       },
     },
     {
@@ -54,6 +54,7 @@ module.exports = {
               aliases: {
                 sh: 'bash',
               },
+              showLineNumbers: true,
             },
           },
           {
