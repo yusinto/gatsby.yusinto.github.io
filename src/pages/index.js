5d634f9e34eaf0676d3e4703cfd200e7af6a3275
Yus <yusinto@gmail.com>
2018-08-30 14:41:29 +1000

    Redesigned landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,136 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link, graphql} from 'gatsby'
+import styled from 'styled-components'
 
-import Layout from '../components/layout'
+const Hero = styled.div`
+  width: 100%;
+  background: forestgreen;
+  height: 265px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Logo = styled.h1`
+  font-size: 55px !important;
+  font-weight: bold;
+  color: white;
+  
+  @media(min-width: 375px) { 
+    font-size: 32px;
+  } 
+`
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <p>
-      Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.
+const PostDescription = styled.div`
+  margin: 10px 20px;
+  
+  &:hover {
+    opacity: 0.7;
+  }
+  
+  @media(min-width: 690px) {
+    margin: 40px 40px;
+  }
+`
+const Title = styled.h3`
+  @media(min-width: 690px) {
+    font-size: 32px;
+  }
+`
+const Date = styled.div`
+  font-size: 14px;
+  
+  @media(min-width: 690px) {
+    font-size: 20px;
+  }
+`
+const Footer = styled.div`
+  height: 200px;
+  width: 100%;
+  margin-top: 40px;
+  background: forestgreen;
+`
+const GatsbyLink = ({className, path, children}) => (
+  <Link className={className} to={path}>{children}</Link>
+);
+const StyledGatsbyLink = styled(GatsbyLink)`{
+  width: 100%;
+  height: 280px;
+  background-image: linear-gradient(
+      rgba(0, 0, 0, 0.2),
+      rgba(0, 0, 0, 0.3)
+    ), 
+    url('${props => props.backgroundImage}');
+  margin-top: 20px;
+  background-position: 50% 50%;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  color: white;
+  
+  &:hover {
+    opacity: 1;
+  }
+  
+  @media(min-width: 690px) {
+    height: 390px;
+  }
+}`;
 
-      Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.
+export default ({data}) => {
+  const posts = data.allMarkdownRemark.edges;
 
-      Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.
+  return (
+    <React.Fragment>
+      <Hero>
+        <Logo>React Junkie</Logo>
+      </Hero>
+      {
+        posts.map(p => {
+          const {frontmatter: {title, date, path, files}} = p.node;
+          const postHero = files && files.find(f => f.name === 'hero');
 
-      Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.
-    </p>
-  </Layout>
-)
+          if (postHero) {
+            return (
+              <StyledGatsbyLink path={path} backgroundImage={postHero.publicURL}>
+                <PostDescription>
+                  <Title>
+                    {title}
+                  </Title>
+                  <Date>{date}</Date>
+                </PostDescription>
+              </StyledGatsbyLink>
+            );
+          }
 
-export default IndexPage
+          // TODO: use default hero if none is set
+          return null;
+        })
+      }
+      <Footer/>
+    </React.Fragment>
+  );
+};
+
+export const indexQuery = graphql`
+    query allPosts {
+        allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            limit: 10
+        ) {
+            edges {
+                node {
+                    frontmatter {
+                        date
+                        path
+                        title
+                        files {
+                            name
+                            publicURL
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
